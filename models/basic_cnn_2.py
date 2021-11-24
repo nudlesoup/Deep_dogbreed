523e3b18a85d9508d72b301d22214ad4b956e3c7
nudlesoup <ameyad1995@gmail.com>
2021-11-24 16:22:59 -0700

    fixed-model

--- models/basic_cnn_2.py
+++ models/basic_cnn_2.py
@@ -1,3 +1,10 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from torch.nn.utils.rnn import pack_padded_sequence
+import torch.nn.functional as F
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 class DogNet(nn.Module):
     ### TODO: choose an architecture, and complete the class
     def __init__(self):
