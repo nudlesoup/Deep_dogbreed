ebf48d810607ee98ab4ce8437635d8079eeeec8f
nudlesoup <ameyad1995@gmail.com>
2021-12-04 06:11:17 -0700

    resnet-og-matrix

--- resnet-train.py
+++ resnet-train.py
@@ -139,9 +139,9 @@ def main(args):
                                                         stratify=y_valid)
     #transforms.Grayscale(num_output_channels=3)
     transform_train = transforms.Compose([
-            # transforms.RandomRotation(20),
-            # transforms.RandomResizedCrop(224),
-            # transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(20),
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
