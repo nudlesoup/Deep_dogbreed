d7e4a5c3e2ae1c4b4614785ad0fa15e40c429a6d
nudlesoup <ameyad1995@gmail.com>
2021-12-04 06:10:26 -0700

    resnet-og-matrix

--- resnet-train.py
+++ resnet-train.py
@@ -147,8 +147,8 @@ def main(args):
         ])
 
     transform_test = transforms.Compose([
-            # transforms.Resize(256),
-            # transforms.CenterCrop(224),
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
