f1c2bc11a6b2f463131db54d40bc759d71c6e8e5
nudlesoup <ameyad1995@gmail.com>
2021-12-05 09:42:13 -0700

    complex-model

--- vgg-train.py
+++ vgg-train.py
@@ -17,7 +17,7 @@ import dataloader
 import matplotlib.pyplot as plt
 import torchvision.models as models
 from sklearn.metrics import confusion_matrix, classification_report
-
+from sklearn.metrics import plot_confusion_matrix
 np.set_printoptions(threshold=np.inf)
 SEED = 1234
 
@@ -28,34 +28,34 @@ torch.cuda.manual_seed(SEED)
 torch.manual_seed(7)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=90)
-    plt.yticks(tick_marks, classes)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-    thresh = cm.max() / 2.
-    # for i, j in itertools.product(range(5), range(5)):
-    #     plt.text(j, i, cm[i, j],
-    #              horizontalalignment="center",
-    #              color="white" if cm[i, j] > thresh else "black")
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.savefig('vgg-confusionmatrix-normal-25.png'.format(args.name))
+# def plot_confusion_matrix(cm, classes,
+#                           normalize=False,
+#                           title='Confusion matrix',
+#                           cmap=plt.cm.Blues):
+#     plt.imshow(cm, interpolation='nearest', cmap=cmap)
+#     plt.title(title)
+#     plt.colorbar()
+#     tick_marks = np.arange(len(classes))
+#     plt.xticks(tick_marks, classes, rotation=90)
+#     plt.yticks(tick_marks, classes)
+#
+#     if normalize:
+#         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+#
+#     thresh = cm.max() / 2.
+#     # for i, j in itertools.product(range(5), range(5)):
+#     #     plt.text(j, i, cm[i, j],
+#     #              horizontalalignment="center",
+#     #              color="white" if cm[i, j] > thresh else "black")
+#     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+#         plt.text(j, i, cm[i, j],
+#                  horizontalalignment="center",
+#                  color="white" if cm[i, j] > thresh else "black")
+#
+#     plt.tight_layout()
+#     plt.ylabel('True label')
+#     plt.xlabel('Predicted label')
+#     plt.savefig('vgg-confusionmatrix-normal-25.png'.format(args.name))
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -275,7 +275,8 @@ def main(args):
         # # f.write(str(confusion_mat))
         # f.close()
         #print(confusion_mat)
-        plot_confusion_matrix(cm=confusion_mat,classes=classes)
+        cfm_plot = plot_confusion_matrix(cm=confusion_mat,classes=classes)
+        cfm_plot.savefig("cfm.png")
         # plot_confusion_matrix(cm=confusion_matrix(y_true=all_label, y_pred=all_pred),
         #                       classes=classes)
         print('Test Accuracy of the model on the 2000 test images: {} %'.format(100 * correct / total))
